f85471181751d31d04261b9b87dec403f2d68be4
Andreas Müller <schnitzeltony@gmail.com>
2020-04-08 12:12:32 +0200

    atmel.cpp: Follow zera-classes rename mGetText -> readVariableLenText
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- atmel.cpp
+++ atmel.cpp
@@ -52,7 +52,7 @@ cATMEL::cATMEL(QString devnode, quint8 adr, quint8 debuglevel) :
 
 ZeraMcontrollerBase::atmelRM cATMEL::readSerialNumber(QString& answer)
 {
-    return mGetText(hwGetSerialNr, answer);
+    return readVariableLenText(hwGetSerialNr, answer);
 }
 
 
@@ -79,13 +79,13 @@ ZeraMcontrollerBase::atmelRM cATMEL::writeSerialNumber(QString &sNumber)
 
 ZeraMcontrollerBase::atmelRM cATMEL::readDeviceName(QString& answer)
 {
-    return mGetText(hwGetDevName, answer);
+    return readVariableLenText(hwGetDevName, answer);
 }
 
 
 ZeraMcontrollerBase::atmelRM cATMEL::readPCBVersion(QString& answer)
 {
-    return mGetText(hwGetPCBVersion, answer);
+    return readVariableLenText(hwGetPCBVersion, answer);
 }
 
 
@@ -112,13 +112,13 @@ ZeraMcontrollerBase::atmelRM cATMEL::writePCBVersion(QString &sVersion)
 
 ZeraMcontrollerBase::atmelRM cATMEL::readCTRLVersion(QString& answer)
 {
-    return mGetText(hwGetCtrlVersion, answer);
+    return readVariableLenText(hwGetCtrlVersion, answer);
 }
 
 
 ZeraMcontrollerBase::atmelRM cATMEL::readLCAVersion(QString& answer)
 {
-    return mGetText(hwGetLCAVersion, answer);
+    return readVariableLenText(hwGetLCAVersion, answer);
 }
 
 
