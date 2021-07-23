646080e487bb10de339cf3a4b84bd3815ad40997
Andreas Müller <schnitzeltony@gmail.com>
2021-07-23 17:15:46 +0200

    cATMELSysCtrl: Add a class to handle communication to atmel system-controller
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ atmelsysctrl.h
@@ -0,0 +1,22 @@
+#ifndef ATMELSYS_H
+#define ATMELATMELSYS_H
+
+#include <QString>
+#include <intelhexfileio.h>
+#include <crcutils.h>
+#include <protocol_zera_bootloader.h>
+#include <protocol_zera_hard.h>
+#include <zera_mcontroller_base.h>
+
+
+class cATMELSysCtrl : public ZeraMcontrollerBase
+{
+public:
+    cATMELSysCtrl(QString devnode, quint8 adr, quint8 debuglevel);
+
+    atmelRM readCTRLVersion(QString& answer);
+};
+
+extern cATMELSysCtrl* pAtmelSys;
+
+#endif // ATMELATMELSYS_H
