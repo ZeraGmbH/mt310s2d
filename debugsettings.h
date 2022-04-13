53fc1b3f732f8ac8374ec52c92ac0ed8c5b28f31
Andreas Müller <schnitzeltony@gmail.com>
2022-04-14 00:21:51 +0200

    debugsettings: Just simplify
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- debugsettings.h
+++ debugsettings.h
@@ -1,8 +1,8 @@
 #ifndef DEBUGSETTINGS_H
 #define DEBUGSETTINGS_H
 
-#include <QObject>
 #include "xmlsettings.h"
+#include <xmlconfigreader.h>
 
 namespace DebugSettings
 {
@@ -12,25 +12,14 @@ enum debugconfigstate
 };
 }
 
-namespace Zera
-{
-namespace XMLConfig
-{
-    class cReader;
-}
-}
-
-
 class cDebugSettings: public cXMLSettings
 {
     Q_OBJECT
 public:
     cDebugSettings(Zera::XMLConfig::cReader *xmlread);
     quint8 getDebugLevel();
-
 public slots:
     virtual void configXMLInfo(QString key);
-
 private:
     quint8 m_nDebugLevel;
 };
