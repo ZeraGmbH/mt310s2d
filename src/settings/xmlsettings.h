4ca6df0c7a248cda09171a47d5289c98573a99ec
Andreas Müller <schnitzeltony@gmail.com>
2022-04-15 15:19:24 +0200

    First steps onto unittests
    To avoid duplication we create a static library from all sources except
    main.cpp. This makes a stub cXMLSettings implementation necessary
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/settings/xmlsettings.h
+++ src/settings/xmlsettings.h
@@ -1,34 +1,23 @@
 #ifndef XMLSETTINGS_H
 #define XMLSETTINGS_H
 
-#include <QObject>
-#include <QMap>
+#include <xmlconfigreader.h>
 #include <QString>
-
-
-namespace Zera
-{
-namespace XMLConfig
-{
-    class cReader;
-}
-}
-
+#include <QMap>
 
 /**
   @brief
   cXMLSettings is pure virtual class, all derived classes will xml configuration facilitiy
  */
-
 class cXMLSettings: public QObject
 {
     Q_OBJECT
-
+public:
+    cXMLSettings();
 public slots:
     virtual void configXMLInfo(QString key) = 0;
-
 protected:
-    QMap<QString,quint16> m_ConfigXMLMap;
+    QMap<QString, quint16> m_ConfigXMLMap;
     Zera::XMLConfig::cReader* m_pXMLReader;
 };
 
