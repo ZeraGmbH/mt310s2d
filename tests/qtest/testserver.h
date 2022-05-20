798c2fdf6f35ca6a06013022eb8ffc32ec923606
Andreas Müller <schnitzeltony@gmail.com>
2022-05-20 23:11:04 +0200

    Get our TestServer more close to to what PCBServer does
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- tests/qtest/testserver.h
+++ tests/qtest/testserver.h
@@ -2,15 +2,25 @@
 #define TESTSERVER_H
 
 #include "scpiconnection.h"
+#include "notificationstring.h"
 
 class TestServer: public cSCPIConnection
 {
+    Q_OBJECT
 public:
     TestServer();
     cSCPI *getSCPIInterface();
+    void addScpiInterface(cSCPIConnection* connection);
     void executeScpiCmd(QString cmd);
+
+    // mandatory overrides - not used...
     void initSCPIConnection(QString leadingNodes) override;
     void executeCommand(int cmdCode, cProtonetCommand* protoCmd) override;
+signals:
+    void sigSendAnswer(QString answer);
+private slots:
+    void sendAnswer(cProtonetCommand* protoCmd);
+    void establishNewNotifier(cNotificationString* notifier);
 };
 
 #endif // TESTSERVER_H
