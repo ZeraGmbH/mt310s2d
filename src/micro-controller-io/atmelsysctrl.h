76a9fb4a7b2d4fc8d4603dd3bd12f24c66af1b12
Andreas Müller <schnitzeltony@gmail.com>
2022-05-20 10:52:07 +0200

    micro-controller-io: Introduce abstract interface for microcontroller
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/micro-controller-io/atmelsysctrl.h
+++ src/micro-controller-io/atmelsysctrl.h
@@ -7,14 +7,14 @@
 #include <protocol_zera_bootloader.h>
 #include <protocol_zera_hard.h>
 #include <zera_mcontroller_base.h>
+#include "atmelbaseinterface.h"
 
-
-class cATMELSysCtrl : public ZeraMcontrollerBase
+class cATMELSysCtrl : public ZeraMcontrollerBase, public AtmelBaseInterface
 {
 public:
     cATMELSysCtrl(QString devnode, quint8 adr, quint8 debuglevel);
 
-    atmelRM readCTRLVersion(QString& answer);
+    atmelRM readCTRLVersion(QString& answer) override;
 };
 
 extern cATMELSysCtrl* pAtmelSys;
