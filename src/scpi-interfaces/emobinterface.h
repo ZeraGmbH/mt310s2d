d5db24e11d877a87cf2c124daaa9edc756e77d43
Andreas Müller <schnitzeltony@gmail.com>
2022-05-20 19:14:32 +0200

    EmobInterface: Add an initial command / prepare second
    In order to understand what it takes to make a first test work, we need a first
    command.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/scpi-interfaces/emobinterface.h
+++ src/scpi-interfaces/emobinterface.h
@@ -4,18 +4,27 @@
 #include "mt310s2d.h"
 #include "scpiconnection.h"
 #include "notificationstring.h"
-
+#include "protonetcommand.h"
 
 class EmobInterface: public cSCPIConnection
 {
 public:
-    EmobInterface(cI2CSettings *i2cSettings);
+    enum SystemCommands
+    {
+        cmdChannelCat,
+        cmdVersionCTRL,
+    };
+
+    EmobInterface(cI2CSettings *i2cSettings, cSCPI* pSCPIInterface);
     void initSCPIConnection(QString leadingNodes) override;
     void actualizeClampStatus(quint16 devConnectedMask);
 protected slots:
     void executeCommand(int cmdCode, cProtonetCommand* protoCmd) override;
 private:
+    QString readChannelCatalog(QString scpiCmd);
+
     cI2CSettings *m_i2cSettings;
+    cNotificationString m_notifierChannelList;
 };
 
 #endif // EMOBINTERFACE_H
